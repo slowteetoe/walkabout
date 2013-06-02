4083a0948eccab53ee4ef28c8d342db43149a6d8
Steven Lotito <steven.lotito@alumni.pitt.edu>
2013-06-01 17:40:48 -0700

    Works with existing data

---
+++ lib/business_license.rb
@@ -0,0 +1,12 @@
+
+class BusinessLicense
+
+	attr_accessor :name, :lat, :lon
+	
+	def initialize(name, lat, lon)
+      @name = name
+      @lat = lat
+      @lon = lon
+	end
+
+end
---
+++ tt.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'pg'
+require 'sinatra/json'
+require 'data_mapper'
+require 'json'
+require 'active_support/all'
+
+DataMapper::Logger.new($stdout, :debug)
+
+DataMapper.setup(:default, 'postgres://tomorrow_today:@127.0.0.1/tomorrow_today')
+
+get '/' do
+  erb :index
+end
+
+# e.g. http://localhost:4567/activity/36.165611999999996/-115.14102100000001/closest/10
+get '/activity/:lat/:lon/closest/:max' do
+  content_type :json
+  r = repository(:default).adapter.select(
+  	"select loc.lat, loc.lon, info.*, earth_distance(earth_pos, ll_to_earth(?, ?)) * 0.000621371192  AS dist_mi from business_license_loc loc JOIN business_license_info info ON info.id = loc.business_license_info_id  where lat <> 0 order by dist_mi asc LIMIT ?",
+  	params[:lat],
+  	params[:lon],
+  	params[:max]
+  	)
+  r.to_json
+end
+
